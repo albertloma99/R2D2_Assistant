d5228ad40ba54b4e3792e7f825167f359775942b
Albert <aloma@edu.tecnocampus.cat>
2021-03-11 20:06:24 +0100

    patata xdxd amos ledss

--- VoiceComands/ConcreteCommands/cmdTurnOnLed.py
+++ VoiceComands/ConcreteCommands/cmdTurnOnLed.py
@@ -1,7 +1,12 @@
 import r2d2
 
 class cmdTurnOnLed(object):
-
+    ledDict = {
+                  "azul":"blue",
+                  "red":"rojo",
+                  "verde":"green",
+                  "superior":"blueup",
+    }
     invokeList = ["enciende led", "enciende luz"]
 
     def __init__(self):
@@ -9,4 +14,11 @@ class cmdTurnOnLed(object):
 
     def executeCmd(self, *args):
         print('TEXTO DE VOZ: '+ args[0])
-        r2d2.turnLed('blue')
+        r2d2.turnLed(self.getColorFromText(args[0]))
+
+    def getColorFromText(self, text):
+        for color in self.ledDict.keys():
+            if(color in text):
+                return self.ledDict[color]
+                break
+
